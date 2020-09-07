c52bfd62425b7578c253c8f2f3512a2c113e9d68
Jung Sim <tlawjd99gma@gmail.com>
2020-09-07 22:50:53 +0900

    initial commit

---
+++ src/p60057.java
@@ -0,0 +1,24 @@
+
+public class p60057 {
+
+	public static void main(String[] args){
+	}
+	
+	public static int solution(String s) {
+		int answer = 0;
+		
+		String temp;
+		for(int i=1; i<=s.length()/2; i++) {
+			for(int j=0; j<s.length() - i; j++) {
+				int cnt = 0;
+				temp = s.substring(j, j+i);
+				
+				for(int k=j+i; k<)
+				System.out.println(temp);
+			}
+		}
+		
+		return answer;
+	}
+
+}
