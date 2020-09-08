0906440d194db65366c9b951d2baef287e9e1687
Jung Sim <tlawjd99gma@gmail.com>
2020-09-09 00:17:00 +0900

    p42888 solved

--- src/p42888.java
+++ src/p42888.java
@@ -22,30 +22,50 @@ public class p42888 {
 	
 	public static String[] solution(String[] record) {
 		
-        ArrayList<String> temp = new ArrayList<>();
+        ArrayList<String[]> chatLog = new ArrayList<>();
         HashMap<String, Info> hm = new HashMap<>(record.length);
         int logNum = 0;
         for(int i=0; i<record.length; i++) {
         	StringTokenizer st = new StringTokenizer(record[i]);
         	String cmd = st.nextToken();
         	String id = st.nextToken();
-        	String name = st.nextToken();
+        	
+        	String name = "";
+        	if(st.hasMoreTokens()) {
+        		name = st.nextToken();
+        	}
         	
         	if(cmd.equals("Enter")){
         		if(!hm.containsKey(id)) {
             		Info info = new Info(name);
             		info.arrList.add(logNum++);
             		hm.put(id, info);
+            	}else {
+            		hm.get(id).name = name;
+            		ArrayList<Integer> alist = hm.get(id).arrList;
+            		for(int j=0; j<alist.size(); j++) {
+            			chatLog.get(alist.get(j))[0] = name;
+            		}
+            		hm.get(id).arrList.add(logNum++);
             	}
+        		chatLog.add(new String[] {hm.get(id).name,"님이 들어왔습니다."});
         	}else if(cmd.equals("Leave")) {
-        		
+        		hm.get(id).arrList.add(logNum++);
+        		chatLog.add(new String[] {hm.get(id).name,"님이 나갔습니다."});
         	}else {
-        		
+        		hm.get(id).name = name;
+        		ArrayList<Integer> alist = hm.get(id).arrList;
+        		for(int j=0; j<alist.size(); j++) {
+        			chatLog.get(alist.get(j))[0] = name;
+        		}
         	}
         	
         }
         
-        String[] answer = new String[temp.size()];
-        return temp.toArray(answer);
+        String[] answer = new String[chatLog.size()];
+        for(int i=0; i<chatLog.size(); i++) {
+        	answer[i] = chatLog.get(i)[0] + chatLog.get(i)[1];
+        }
+        return answer;
     }
 }
